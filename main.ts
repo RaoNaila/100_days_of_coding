eacbbb26a9955defabb118dcd2df140c2de7f9bd
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-05-19 12:15:23 -0700

    2 days completed

---
+++ main.ts
@@ -0,0 +1,55 @@
+//***************************************************************************************** */
+
+//////////////starting 100 day of coding//////////////////////////
+
+//*****************************DAY FIRST********************************************* */
+                 //================QUESTION 1===================//
+
+//   console.log("Hello world.")
+
+                  //================QUESTION 2   ===================//
+
+//  let personalMessage:string="salam Naila , would you like to learn some typescript today"
+//  console.log(personalMessage);
+
+                     //================QUESTION  3 ===================//
+ 
+//let personName:string="nAIla "
+//change in lowercase
+// personName.toLocaleLowerCase()
+// console.log(personName.toLocaleLowerCase());
+
+//change in upppercase
+
+// personName.toLocaleUpperCase()
+// console.log(personName.toLocaleUpperCase());
+
+// //change in title case
+
+// personName.charAt(0).toLocaleUpperCase()+ personName.slice(1).toLocaleLowerCase()
+// console.log(personName.charAt(0).toLocaleUpperCase()+ personName.slice(1).toLocaleLowerCase());
+
+//**********************************complete day first************************************** */
+
+
+//*****************************DAY second********************************************* */
+
+       
+                     //================QUESTION  4===================//
+// famous person quote
+
+//console.log(`'Albert Einstine once said,"A person who never made a mistake never tried anything new."`);
+
+                      //================QUESTION  5===================//
+// let famous_person:string="Alber Eienstein";
+// let message:string="A person who never made a mistake never tried anything new.";
+// console.log(`'${famous_person} oncce said,"${message}"'`);
+     
+                      //================QUESTION  6===================//
+
+// let myName:string="\t\n Naila\t\n"
+// console.log(myName);
+// console.log(myName.trim());
+
+//**********************************complete day second************************************** */
+
