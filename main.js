4cb2c2306f48bd6e7dd461c82f5c8aebe73e0621
RaoNaila <160322743+RaoNaila@users.noreply.github.com>
2024-05-28 23:20:17 -0700

    11 day complete

--- main.js
+++ main.js
@@ -307,13 +307,87 @@
 //  };
 //==========================QUESTION 30 ====================================//
 //special grreting for admin in loop
-let useres = ["Admin", "user1", "user2", "user3"];
-for (let user of useres) {
-    if (user === "Admin") {
-        console.log("Hello Admin ! how are you");
+// let useres:string[]=["Admin","user1","user2","user3"];
+// for(let user of useres){
+//   if(user === "Admin"){
+//     console.log("Hello Admin ! how are you");
+//   }else{
+//     console.log("welcome in web page");
+//   }
+// };
+//************************************** 10th day completed****************************//
+//************************************** 11th day completed****************************//
+//=======================Question 31===========================//
+// - No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// If the list is empty, print the message We need to find some users!
+// Remove all of the usernames from your array, and make sure the correct message is printed
+// let users:string[]=["Admin","user1","user2","user3"];
+// if(users.length===0){
+//   console.log("list is empty");
+// }else{
+//   console.log("there are some user");
+// };
+// //now empty user
+// users=[];
+// if(users.length === 0){
+//   console.log("we need some users");
+// }else{
+//   console.log("hello");
+// };
+//=======================Question 32===========================//
+// Q32 - Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// Make a list of five or more usernames called current_users.
+// Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+// Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+//create 2 arrats of same name at least two same name
+// let user1:string[]=["Naila","shabna","Farhana","farzana","nosheen"];
+// let user2:string[]=["Naila","BUSHARA","Farzana","Rehan","shaista"];
+// user2.forEach((user)=>{
+// if(user1.some(
+//   (users)=>users.toLocaleLowerCase()===user.toLocaleLowerCase()
+// )){
+//   console.log(`${user} need to enter a newname.`);
+// }else{
+//   console.log(`${user} is available`);
+// };
+// });
+//==============================Question 33=============================//
+//     Q33 - Ordinal Numbers: Ordinal numbers indicate their position in a array, 
+//such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// Store the numbers 1 through 9 in a array.
+// Loop through the array.
+// Use an if-else chain inside the loop to print the proper ordinal ending for each number.
+// Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", 
+//and each result should be on a separate line.
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let ordinalnumber of numbers){
+//   if(ordinalnumber === 1){
+//     let number= ordinalnumber+"st"
+//     console.log(number);
+//   }if(ordinalnumber===2){
+//     let number=ordinalnumber+"nd"
+//     console.log(number);
+//   }if(ordinalnumber===3){
+//     let number =ordinalnumber+"rd"
+//     console.log(number);
+//   }else{
+//     let number=ordinalnumber+"th"
+//     console.log(number);
+//   }
+// };
+//through foreach
+numbers.forEach((number) => {
+    let suffix = "th";
+    if (number === 1) {
+        suffix = "st";
     }
-    else {
-        console.log("welcome in web page");
+    else if (number === 2) {
+        suffix = "nd";
     }
-}
+    else if (number === 3) {
+        suffix = "rd";
+    }
+    console.log(`${number}${suffix}`);
+});
 export {};
